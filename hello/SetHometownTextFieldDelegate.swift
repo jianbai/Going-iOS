a0620615eb9bb94252d24996ce72064af79c9dab
scott <scott_wang@alumni.brown.edu>
2015-02-01 08:02:43 -0800

    started implemented SetProfileViewController

---
+++ hello/SetHometownTextFieldDelegate.swift
@@ -0,0 +1,47 @@
+//
+//  SetHometownTextFieldDelegate.swift
+//  hello
+//
+//  Created by scott on 2/1/15.
+//  Copyright (c) 2015 spw. All rights reserved.
+//
+
+import Foundation
+
+class SetHometownTextFieldDelegate: NSObject, UITextFieldDelegate {
+    
+    let movementDistance: CGFloat = 80
+    let movementDuration = 0.3
+    var viewController: SetProfileViewController
+    
+    init(viewController: SetProfileViewController) {
+        self.viewController = viewController
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        animateTextField(textField, up: true)
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        animateTextField(textField, up: false)
+    }
+    
+    func animateTextField(textField: UITextField, up: Bool) {
+        
+        var movement: CGFloat = (up ? -movementDistance : movementDistance)
+        
+        UIView.beginAnimations("anim", context: nil)
+        UIView.setAnimationBeginsFromCurrentState(true)
+        UIView.setAnimationDuration(movementDuration)
+        viewController.view.frame = CGRectOffset(viewController.view.frame, 0, movement)
+        
+        UIView.commitAnimations()
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        viewController.view.endEditing(true)
+        return false
+    }
+    
+}
--- hello/SetProfileViewController.swift
+++ hello/SetProfileViewController.swift
@@ -9,15 +9,45 @@
 import UIKit
 
 class SetProfileViewController: UIViewController {
+    @IBOutlet weak var birthdayTextField: UITextField!
+    @IBOutlet weak var hometownTextField: UITextField!
+    @IBOutlet weak var genderTextField: UITextField!
+  
+    var noGender: Bool! = true
+    var noAge: Bool! = true
+    var noHometown: Bool! = false
+    var gender: String?
+    var age: String?
+    var hometown: String?
     
-    var noGender: Bool!
-    var noAge: Bool!
-    var noHometown: Bool!
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if (!self.noAge) {
+            self.birthdayTextField.hidden = true
+        }
+        if (!self.noHometown) {
+            self.hometownTextField.hidden = true
+        }
+        if (!self.noGender) {
+            self.genderTextField.hidden = true
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.birthdayTextField.returnKeyType = UIReturnKeyType.Done
+//        self.birthdayTextField.delegate = self
+//        self.hometownTextField.delegate = self
+//        self.genderTextField.delegate = self
+        self.hometownTextField.returnKeyType = UIReturnKeyType.Done
+        self.genderTextField.returnKeyType = UIReturnKeyType.Done
+        self.birthdayTextField.delegate = SetAgeTextFieldDelegate(viewController: self)
+        self.hometownTextField.delegate = SetHometownTextFieldDelegate(viewController: self)
+        self.genderTextField.delegate = SetGenderTextFieldDelegate(viewController: self)
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
