2ef0215fba324519abaa065e1e3dce1f9d8f960c
scott <scott_wang@alumni.brown.edu>
2015-02-04 17:20:14 -0800

    completed FaqViewController

---
+++ hello/RaqViewController.swift
@@ -0,0 +1,14 @@
+//
+//  RaqViewController.swift
+//  hello
+//
+//  Created by scott on 2/4/15.
+//  Copyright (c) 2015 spw. All rights reserved.
+//
+
+import UIKit
+
+class RaqViewController: UIViewController {
+    
+    
+}
